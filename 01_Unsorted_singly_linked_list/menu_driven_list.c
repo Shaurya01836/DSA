fbf0a83d9e87bb5a15c7a7accf9d2b468d2ef113
Shaurya Upadhyay <shaurya01836@gmail.com>
2024-08-26 18:59:03 +0530

    menu driven unsorted singly linked list update

--- 01_Unsorted_singly_linked_list/menu_driven_list.c
+++ 01_Unsorted_singly_linked_list/menu_driven_list.c
@@ -1,4 +1,4 @@
-//Menu driven
+// Menu driven
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +8,7 @@ struct singly_linked_list
     struct singly_linked_list *next;
 };
 
+// function for creating a n no. of linked list
 struct singly_linked_list *create(struct singly_linked_list *start, int n)
 {
     struct singly_linked_list *p, *q;
@@ -38,18 +39,7 @@ struct singly_linked_list *create(struct singly_linked_list *start, int n)
     return start;
 }
 
-void traverse(struct singly_linked_list *start)
-{
-    struct singly_linked_list *p;
-    p = start;
-    while (p != NULL)
-    {
-        printf("%d -> ", p->data);
-        p = p->next;
-    }
-    printf("NULL\n");
-}
-
+// function for counting no. of linked list
 int count(struct singly_linked_list *start)
 {
     struct singly_linked_list *p;
@@ -63,6 +53,47 @@ int count(struct singly_linked_list *start)
     return c;
 }
 
+// function for inserting a value at beginning
+struct singly_linked_list *insertion(struct singly_linked_list *start)
+{
+    struct singly_linked_list *p;
+    int v;
+    p = (struct singly_linked_list *)malloc(sizeof(struct singly_linked_list));
+    printf("Enter the value of v :");
+    scanf("%d", &v);
+    p->data = v;
+    p->next = start;
+    start = p;
+    return start;
+}
+
+// function for inserting a value at the end
+struct singly_linked_list *insertion_at_end(struct singly_linked_list *start)
+{
+    struct singly_linked_list *p, *q;
+    int v;
+    p = (struct singly_linked_list *)malloc(sizeof(struct singly_linked_list));
+    printf("Enter the value of v :");
+    scanf("%d", &v);
+    p->data = v;
+    p->next = NULL;
+    if (start == NULL)
+    {
+        start = p;
+    }
+    else
+    {
+        q = start;
+        while (q->next != NULL)
+        {
+            q = q->next;
+        }
+        q->next = p;
+    }
+    return start;
+}
+
+// function for inserting a value at a specific position
 struct singly_linked_list *insertion_at_position(struct singly_linked_list *start)
 {
     int n, pos, val;
@@ -78,8 +109,8 @@ struct singly_linked_list *insertion_at_position(struct singly_linked_list *star
     }
     else
     {
-        p = start;  // Initialize p to the start of the list
-        for (int i = 1; i < (pos - 1); i++)  // Traverse the list to the (pos-1)th node
+        p = start;
+        for (int i = 1; i < (pos - 1); i++)
         {
             p = p->next;
         }
@@ -94,35 +125,126 @@ struct singly_linked_list *insertion_at_position(struct singly_linked_list *star
     return start;
 }
 
+// function for deleting a value at beginning
+struct singly_linked_list *delete_at_beginnning(struct singly_linked_list *start)
+{
+    struct singly_linked_list *p;
+    if (start == NULL)
+    {
+        printf("Empty list");
+    }
+    else
+    {
+        p = start;
+        start = start->next;
+        free(p);
+    }
+    return start;
+}
+
+// function for deleting a value at end
+struct singly_linked_list *delete_at_end(struct singly_linked_list *start)
+{
+    struct singly_linked_list *p, *q;
+    p = start;
+    while (p->next != NULL)
+    {
+        q = p;
+        p = p->next;
+    }
+    free(p);
+    q->next = NULL;
+    return start;
+}
+
+// function for deleting a value
+struct singly_linked_list *delete_at_any_pos(struct singly_linked_list *start)
+{
+    struct singly_linked_list *p, *q;
+    int value;
+    printf("Enter the value which you want to delete :");
+    scanf("%d", &value);
+    p = start;
+    if (p->data == value)
+    {
+        start = p->next;
+        free(p);
+        return start;
+    }
+    while (p->data != value)
+    {
+        q = p;
+        p = p->next;
+        if (p == NULL)
+        {
+            printf("value not found\n");
+            return start;
+        }
+    }
+    q->next = p->next;
+    free(p);
+    return start;
+}
+
+// function for printing the linked list
+void traverse(struct singly_linked_list *start)
+{
+    struct singly_linked_list *p;
+    p = start;
+    while (p != NULL)
+    {
+        printf("%d -> ", p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
+
 int main()
 {
     struct singly_linked_list *start = NULL;
     int choice, n;
+    printf("Enter the number of nodes (n): ");
+    scanf("%d", &n);
+    start = create(start, n);
 
     while (1)
     {
         printf("\nMenu:\n");
-        printf("1. Create Linked List\n");
-        printf("2. Insert Node at Position\n");
-        printf("3. Traverse Linked List\n");
-        printf("4. Exit\n");
+        printf("1. Insert Node at beginning\n");
+        printf("2. Insert Node at end\n");
+        printf("3. Insert Node at Position\n");
+        printf("4. Delete Node at beginning\n");
+        printf("5. Delete Node at end\n");
+        printf("6. Delete Node at Position\n");
+        printf("7. Traverse Linked List\n");
+        printf("8. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
-            printf("Enter the number of nodes (n): ");
-            scanf("%d", &n);
-            start = create(start, n);
+            start = insertion(start);
             break;
         case 2:
-            start = insertion_at_position(start);
+            start = insertion_at_end(start);
             break;
         case 3:
-            traverse(start);
+            start = insertion_at_position(start);
             break;
         case 4:
+            start = delete_at_beginnning(start);
+            break;
+        case 5:
+            start = delete_at_end(start);
+            break;
+        case 6:
+            start = delete_at_any_pos(start);
+            traverse(start);
+        case 7:
+            traverse(start);
+            break;
+        case 8:
             printf("Exiting...\n");
             exit(0);
         default:
---
+++ 05_Circular_singly_list/creation.c
@@ -0,0 +1,61 @@
+// Circular singly unsorted linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+struct circular_list
+{
+    int data;
+    struct circular_list *next;
+};
+struct circular_list *creation(struct circular_list *start, int n)
+{
+    struct circular_list *p, *q;
+    int value;
+    if (n <= 0)
+    {
+        return NULL;
+    }
+    else
+    {
+        p = (struct circular_list *)malloc(sizeof(struct circular_list));
+        printf("Enter the value :");
+        scanf("%d", &value);
+        p->data = value;
+        p->next = p;
+        start = p;
+        for (int i = 2; i <= n; i++)
+        {
+            q = (struct circular_list *)malloc(sizeof(struct circular_list));
+            printf("Enter the value :");
+            scanf("%d", &value);
+            q->data = value;
+            q->next = start;
+            p->next = q;
+            p = p->next;
+        }
+    }
+    return start;
+}
+
+void traverse(struct circular_list *start)
+{
+    struct circular_list *p;
+    p = start;
+    while (p != start)
+    {
+        printf("%d -> ", p->data);
+        p = p->next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d", &n);
+    struct circular_list *start = NULL;
+    start = creation(start, n);
+    traverse(start);
+    return 0;
+}
